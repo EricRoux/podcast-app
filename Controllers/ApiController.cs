cf2b21f16da8db2a66a0d2b3a537fc997298f13d
RobertWells821 <frolenkovda>
2022-07-03 16:10:17 +0300

    add controller newQuestion

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace project1
+{
+    [ApiController]
+    [Route("/api/v1")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public class ApiController : Controller
+    {
+        /// <summary>
+        /// Add new question
+        /// </summary>
+        [HttpPost("newQuestion")]
+        public IActionResult newQuestion([FromBody] Question q){
+            return Ok();
+        }
+    }
+}
