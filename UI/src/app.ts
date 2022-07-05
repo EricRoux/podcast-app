308a8c53955015a027d1f8421318dbe6102795c3
RobertWells821 <frolenkovda>
2022-07-05 21:09:59 +0300

    Move modalBtn to class Modal

--- UI/src/app.ts
+++ UI/src/app.ts
@@ -1,10 +1,9 @@
-import "./styles.scss";
 import { isValidInput } from "./Utils/isValidInput";
 import { IQuestion } from "./Interfaces/IQuestion";
 import { Question } from "./question";
 import { renderList } from "./Utils/renderList";
-import { createModal } from "./Utils/createModal";
-import { getAuthFormHTML, authWithEmailAndPassword } from "./auth";
+import { Modal } from "./modal";
+import "./styles.scss";
 
 const form: HTMLElement = document.querySelector("#form");
 const input: HTMLInputElement = form.querySelector("#question-input");
@@ -34,61 +33,8 @@ function imputChanged(): void {
     submitBtn.disabled = !isValidInput(input.value);
 }
 
-function openModal(className: string): void {
-    createModal(className, "Авторизация", getAuthFormHTML());
-}
-
-function closeModal(className: string): void {
-    document.querySelector(`.${className}`).remove();
-}
-
-function authFormHandler(event: Event): void {
-    event.preventDefault();
-}
-
-function modalEvents(className: string): void {
-    document
-        .querySelector(`.${className}`)
-        .addEventListener("submit", authFormHandler, {once: true});
-}
-
-function modalAuthorization(className: string): void {
-    const email: string = (
-        <HTMLInputElement>document
-            .querySelector(`.${className}`)
-            .querySelector("#email")
-    ).value;
-
-    const password: string = (
-        <HTMLInputElement>document
-            .querySelector(`.${className}`)
-            .querySelector("#email")
-    ).value;
-
-    authWithEmailAndPassword(email, password)
-        .then((token: string): void => {
-            localStorage.setItem("authToken", token);
-            modalBtn.innerText = "o";
-            reloadQuestions();
-        });
-}
-
-function modal(): void {
-    const modalClass: string = "modal";
-    if(modalBtn.innerText == "+"){
-        openModal(modalClass);
-        modalBtn.innerText = "-";
-    } else if(modalBtn.innerText == "-") {
-        closeModal(modalClass);
-        modalBtn.innerText = "+";
-    } else {
-        reloadQuestions();
-    }
-    modalEvents(modalClass);
-    modalAuthorization(modalClass);
-}
-
+const modalHTML: Modal = new Modal(modalBtn);
+modalHTML.createBtnEvents();
 window.addEventListener("load", renderList);
 form.addEventListener("submit", submitFormHandler);
 input.addEventListener("input", imputChanged);
-modalBtn.addEventListener("click", modal);
---
+++ UI/src/modal.ts
@@ -0,0 +1,67 @@
+import { createModal } from "./Utils/createModal";
+import { getAuthFormHTML, authWithEmailAndPassword } from "./auth";
+
+export class Modal {
+
+    private modalBtn: HTMLButtonElement;
+
+    constructor(modalBtn: HTMLButtonElement) {
+        this.modalBtn = modalBtn;
+    }
+
+    openModal(className: string): void {
+        createModal(className, "Авторизация", getAuthFormHTML());
+    }
+    
+    closeModal(className: string): void {
+        document.querySelector(`.${className}`).remove();
+    }
+    
+    authFormHandler(event: Event): void {
+        event.preventDefault();
+        const target: HTMLTextAreaElement = event.target as HTMLTextAreaElement;
+        const email: string = (
+            <HTMLInputElement>document
+                .querySelector(`.${target.className}`)
+                .querySelector("#email")
+        ).value;
+    
+        const password: string = (
+            <HTMLInputElement>document
+                .querySelector(`.${target.className}`)
+                .querySelector("#email")
+        ).value;
+        
+        this.closeModal(target.className);
+        authWithEmailAndPassword(email, password)
+            .then((token: string): void => {
+                localStorage.setItem("authToken", token);
+                this.modalBtn.innerText = "o";
+                // reloadQuestions();
+            });
+    }
+    
+    modalEvents(className: string): void {
+        const modal = document.querySelector(`.${className}`);
+        modal.addEventListener("submit", this.authFormHandler, {once: true});
+        modal.className = className;
+    }
+    
+    modal(): void {
+        const modalClass: string = "modal";
+        if(this.modalBtn.innerText == "+"){
+            this.openModal(modalClass);
+            this.modalBtn.innerText = "-";
+        } else if(this.modalBtn.innerText == "-") {
+            this.closeModal(modalClass);
+            this.modalBtn.innerText = "+";
+        } else {
+            // reloadQuestions();
+        }
+        this.modalEvents(modalClass);
+    }
+
+    createBtnEvents(): void {
+        this.modalBtn.addEventListener("click", this.modal);
+    }
+}
