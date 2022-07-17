831459417b1201c5674c55a94ea10144d1c443a7
Eric Roux <berti481231@gmail.com>
2022-07-17 21:50:27 +0300

    Create test selenium file

---
+++ UI/tests/selenium.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const {Builder, By, Key, until} = require("selenium-webdriver");
+const assert = require("assert");
+const chrome = require("selenium-webdriver/chrome");
+
+async function openChromeTest(): Promise<void> {
+    const options = new chrome.Options();
+    const driver = await new Builder()
+        .usingServer("http://localhost:4444")
+        .setChromeOptions(options)
+        .forBrowser("chrome")
+        .build();
+    try {
+        await driver.get("https://www.google.com");
+        await driver.getTitle();
+        await driver.manage().setTimeouts({implicit: 1000});
+        const searchBox = await driver.findElement(By.name("q"));    
+        await searchBox.sendKeys("Selenium", Key.ENTER);        
+        const firstResult = await driver.wait(
+            until.elementLocated(By.css("h3")),
+            10000
+        );
+        console.log(await firstResult.getAttribute("textContent"));    
+        const value = await firstResult.getAttribute("textContent");
+        assert.deepStrictEqual(value, "Selenium");
+        await driver.quit();
+    } catch (error) {
+        console.log(error);
+        await driver.quit();
+    }
+}
+
+openChromeTest();
